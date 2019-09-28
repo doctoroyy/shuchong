6a3262df7b8b1cd2a739133ae0c5bbebd8ed9fa8
xiaoyu <doctor.oyy@gmail.com>
2019-09-28 18:26:34 +0800

    接口规范化

--- book/urls.py
+++ book/urls.py
@@ -1,15 +1,16 @@
 from django.contrib import admin
 from django.urls import path
 
-from book import views
 
+from book import views
 urlpatterns = [
   path('', views.index, name='index'),
-  path('book/<int:book_id>/<int:chapter_no>/', views.chapter_detail, name='chapter_detail'),
   path('download/', views.download, name='download'),
-  path('catalog/<int:book_id>/', views.catalog, name='catalog'),
-  path('catalogtojson/<int:book_id>/', views.catalogtojson, name='catalogtojson'),
+  path('book/getAll', views.get_all_books, name='get_all_books'),
+  path('book/chapter', views.chapter, name='getChapter'),
+  path('book/catalog', views.catalog, name='getCatalog'),
+  # 历史
+  path('catalog/<int:book_id>/', views.catalog2, name='catalog'),
+  path('book/<int:book_id>/<int:chapter_no>/', views.chapter_detail, name='chapter_detail'),
 
-  path('demo/', views.demo, name='demo'),
-  path('getAll/', views.get_all_books, name='get_all_books')
 ]
--- book/views.py
+++ book/views.py
@@ -8,8 +8,12 @@ from django.conf import settings
 # Create your views here.
 from book import utils
 from book.models import Novel, Chapter
-from book.utils import save_book
+from book.utils import save_book, get_chapter_content
 from main import get_json_data
+from django.views.decorators.csrf import csrf_exempt
+
+from shuchong1.settings import BOOK_SRC_URL
+from django.core.paginator import Paginator
 
 
 def index(request):
@@ -17,21 +21,89 @@ def index(request):
   return render(request, 'novel_list.html', {'books': books})
 
 
-def demo(request):
-  area = request.GET['area']
-  type = request.GET['type']
-  year = request.GET['year']
+def download(request):
+  book_id = request.GET['id']
 
-  filepath = 'films&area=%s&type=%s&year=%s.json' % (area, type, year)
-  # 如果文件不存在
-  if os.path.exists(filepath) is not True:
-    get_json_data(area, type, year)
+  if book_id not in settings.BOOK_DOWNLOAD_QUEUE:
+    settings.BOOK_DOWNLOAD_QUEUE.add(book_id)
+    # print(u'新线程开始....')
+    threading.Thread(target=save_book, args=[settings.BOOK_SRC_URL + '/' + book_id, ]).start()
 
-  # if time.localtime(os.path.getmtime(filepath)).tm_mday != time.localtime(time.time()).tm_mday:
-  #   get_json_data(area, type, year)
-  f = open(filepath, encoding='utf-8')
-  return HttpResponse(f.read(), content_type='application/json')
+  return HttpResponse('downloading...')
 
+
+@csrf_exempt
+def chapter(request):
+  body = json.loads(request.body)
+  bookId = int(body['id'])
+  chapterNo = int(body['chapterno'])
+  spiderUrl = Chapter.objects.get(novel_id=bookId, no=chapterNo).context_url
+  name = Chapter.objects.get(novel_id=bookId, no=chapterNo).name
+  context = get_chapter_content(BOOK_SRC_URL + spiderUrl).split('\xa0\xa0\xa0\xa0')
+
+  data = {
+    'context': context,
+    'name': name
+  }
+  return HttpResponse(json.dumps(data), content_type="application/json")
+
+
+@csrf_exempt
+def catalog(request):
+  bookId = request.GET['id']
+  calalog = Chapter.objects.filter(novel_id=bookId).values()
+  info = Novel.objects.filter(id=bookId).values()[0]
+  data = {
+    'code': 0,
+    'data': {
+      'chapters': list(calalog),
+      'bookInfo': dict(info),
+    }
+  }
+  return HttpResponse(json.dumps(data), content_type="application/json")
+
+
+@csrf_exempt
+def get_all_books(request):
+  body = json.loads(request.body)
+  page = body['page']
+  page_size = body['pageSize']
+  book_list = Novel.objects.all().values()
+  p = Paginator(book_list, page_size)
+  total = book_list.__len__()
+  pages = total / page_size
+  if total % page_size != 0:
+    pages += 1
+  data = {
+    'page': page,
+    'pageSize': page_size,
+    'total': total,
+    'data': p.page(page).object_list
+  }
+
+  return HttpResponse(json.dumps(data))
+
+
+
+# 历史
+def catalog2(request, book_id):
+  bookInfo = Novel.objects.get(id=book_id)
+  chapters = Chapter.objects.filter(novel_id_id=book_id)
+
+  len = chapters.__len__()
+
+  tmp1 = []
+  for chapter in chapters:
+    tmp1.append({'chapter_name': chapter.name, 'src': chapter.context_url})
+
+  data = {}
+  data['chapters'] = tmp1
+  data['info'] = bookInfo.description
+  data['cover'] = bookInfo.imgSrc
+  data['author'] = bookInfo.author
+  data['name'] = bookInfo.name
+  # return HttpResponse(json.dumps(data), content_type="application/json")
+  return render(request, 'chapterlist.html', {'chapters': chapters, 'bookInfo': bookInfo, 'len': len})
 
 def chapter_detail(request, book_id, chapter_no):
   bookInfo = Novel.objects.get(id=book_id)
@@ -56,63 +128,4 @@ def chapter_detail(request, book_id, chapter_no):
 
   return render(request, 'chapter_detail.html', ctx)
 
-
-def download(request):
-  book_id = request.GET['id']
-
-  if book_id not in settings.BOOK_DOWNLOAD_QUEUE:
-    settings.BOOK_DOWNLOAD_QUEUE.add(book_id)
-    # print(u'新线程开始....')
-    threading.Thread(target=save_book, args=[settings.BOOK_SRC_URL + '/' + book_id, ]).start()
-
-  return HttpResponse('downloading...')
-
-
-def catalog(request, book_id):
-  bookInfo = Novel.objects.get(id=book_id)
-  chapters = Chapter.objects.filter(novel_id_id=book_id)
-
-  len = chapters.__len__()
-
-  # tmp1 = []
-  # for chapter in chapters:
-  #   tmp1.append({'chapter_name': chapter.name, 'src': chapter.context_url})
-  #
-  # data = {}
-  # data['chapters'] = tmp1
-  # data['info'] = bookInfo.description
-  # data['cover'] = bookInfo.imgSrc
-  # data['author'] = bookInfo.author
-  # data['name'] = bookInfo.name
-  # return HttpResponse(json.dumps(data), content_type="application/json")
-  return render(request, 'chapterlist.html', {'chapters': chapters, 'bookInfo': bookInfo, 'len': len})
-
-
-def catalogtojson(request, book_id):
-  bookInfo = Novel.objects.get(id=book_id)
-  chapters = Chapter.objects.filter(novel_id_id=book_id)
-
-  len = chapters.__len__()
-
-  tmp1 = []
-  for chapter in chapters:
-    tmp1.append({'chapter_name': chapter.name, 'src': chapter.context_url})
-
-  data = {}
-  data['chapters'] = tmp1
-  data['info'] = bookInfo.description
-  data['cover'] = bookInfo.imgSrc
-  data['author'] = bookInfo.author
-  data['name'] = bookInfo.name
-  return HttpResponse(json.dumps(data), content_type="application/json")
-  # return render(request, 'chapterlist.html', {'chapters': chapters, 'bookInfo': bookInfo, 'len': len})
-
-
-def get_all_books(request):
-  books = Novel.objects.all()
-  data = []
-  for book in books:
-    data.append(
-      {'id': book.id, 'name': book.name, 'cover': book.imgSrc, 'descrip': book.description, 'author': book.author})
-
-  return HttpResponse(json.dumps(data), content_type="application/json")
+# 历史
